8947c053b3a07f1704cc47acf2cee7d0f7c1e49b
Vinicius <Vinicius@DESKTOP-N1SLGIK>
2022-11-28 19:08:49 -0300

    working with four verbs

---
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace RestWithASPNET.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Address { get; set; }
+        public long Gender { get; set; }
+
+    }
+}
